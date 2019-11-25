4f5d03a627313a8d5cfe2c011625c50abf31c1ac
7u83 <7u83@mail.ru>
2019-11-25 20:30:16 +0000

    Updated puppet ssldir params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,24 @@
-
-#params
+#psec::params
 
 class ipsec::params {
 	case $::osfamily {
 		'FreeBSD':{
 			$default_ike_daemon = 'racoon'
+			$openssl_cmd = "/usr/bin/openssl"
 		}
 		'OpenBSD':{
 			$default_ike_daemon = 'isakmpd'
+			$openssl_cmd = "/usr/bin/openssl"
 		}
 		default: {
 			$default_ike_daemon = 'strongswan'
+			$openssl_cmd = "/usr/bin/openssl"
 		}
-	}	
+	}
+	
+	$puppet_ca_cert = "$ipsec_puppet_ssldir/certs/ca.pem"
+	$puppet_client_cert = "$ipsec_puppet_ssldir/certs/${facts[clientcert]}.pem"
+	$puppet_client_key = "$ipsec_puppet_ssldir/private_keys/${facts[clientcert]}.pem"
+	$puppet_crl = "ipsec_$puppet_ssldir/crl.pem"
 }
 
